a38dc82ee5a9e0f77ec3e10c88d3e4ccba2f3302
Insane5819 <93830376+Insane5819@users.noreply.github.com>
2022-01-07 13:06:44 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const server = express();
+
+server.all('/', (req, res)=>{
+    res.send("WAIT 3 MINUTES TO SEE YOUR RPC")
+})
+
+function keepAlive(){
+    server.listen(process.env.PORT, ()=>{console.log("ADITYA CODEZ RPC IS READY")});
+}
+module.exports = keepAlive
