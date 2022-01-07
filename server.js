f89a881e24a747e6c5986c219c6e10dd5f37ca3b
Insane5819 <93830376+Insane5819@users.noreply.github.com>
2022-01-07 17:53:09 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const server = express();
+
+server.all('/', (req, res)=>{
+    res.send("Bot is ready!!")
+})
+
+function keepAlive(){
+    server.listen(process.env.PORT, ()=>{console.log("Insane Codez™")});
+}
+module.exports = keepAlive
